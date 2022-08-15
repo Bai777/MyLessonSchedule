140ef9ad52b008ccf180e801a4ecb50ab4693ed9
bai_777 <bai_777@mail.ru>
2022-08-15 18:59:02 +0300

    добавил FakeRepository.kt, HomeWork.kt, HomeWorkState.kt

--- app/src/main/java/com/example/mylessonschedule/data/Repository.kt
+++ app/src/main/java/com/example/mylessonschedule/data/Repository.kt
@@ -1,5 +1,7 @@
 package com.example.mylessonschedule.data
 
+import com.example.mylessonschedule.domain.HomeWork
+import com.example.mylessonschedule.domain.Class
 import io.reactivex.rxjava3.core.Single
 
 interface Repository {
--- app/src/main/java/com/example/mylessonschedule/data/RepositoryImplFake.kt
+++ app/src/main/java/com/example/mylessonschedule/data/RepositoryImplFake.kt
@@ -1,6 +1,11 @@
 package com.example.mylessonschedule.data
 
-class RepositoryImplFake(private val repo:FakeRepository = FakeRepository()): Repository {
+import com.example.mylessonschedule.domain.FakeRepository
+import com.example.mylessonschedule.domain.HomeWork
+import com.example.mylessonschedule.domain.Class
+import io.reactivex.rxjava3.core.Single
+
+class RepositoryImplFake(private val repo: FakeRepository = FakeRepository()) : Repository {
     override fun getClasses(): Single<List<Class>> {
         return repo.getClasses()
     }
